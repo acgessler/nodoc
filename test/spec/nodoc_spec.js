20ec4dc58da6ccefa04642c59abb20fea3c05695
Alexander Gessler <alexander.gessler@gmx.net>
2013-08-14 00:05:40 +0200

    Updated spec/tests.

--- test/spec/nodoc_spec.js
+++ test/spec/nodoc_spec.js
@@ -1,47 +1,66 @@
 
-describe("nodoc", function() {
+$(document).ready(function() {
+  describe("nodoc", function() {
 
-  var doc;
-  beforeEach(function() {
-    doc = new DocumentationViewer({
-      base_folder : ".."
-      });
-  });
-
-  // ------------------------------------------------------------------------------
-  it("should be able to open the doc for class Object", function() {
-    var ok = false;
-    runs(function() {
-      doc.open_class('../output/class_Object.json', function(result) {
-        ok = result;
-      });
+    var doc;
+    beforeEach(function() {
+      doc = new DocumentationViewer({
+        base_folder : ".."
+        });
     });
 
-    waitsFor(function() {
-      return ok;
-    }, "callback should be called with result=true", 500);
+    // ------------------------------------------------------------------------------
+    it("should be able to open the doc for class Object", function() {
+      var ok = false;
+      runs(function() {
+        doc.open_class('../output/class_Object.json', function(result) {
+          ok = result;
+        });
+      });
 
-    runs(function() {
-      expect(ok).toBe(true);
-    });
-  });
+      waitsFor(function() {
+        return ok;
+      }, "callback should be called with result=true", 500);
 
-
-  // ------------------------------------------------------------------------------
-  it("should not be able to open the doc for class ThisClassDoesNotExist", function() {
-    var ok = false;
-    runs(function() {
-      doc.open_class('../output/class_ThisClassDoesNotExist.json', function(result) {
-        ok = !result;
+      runs(function() {
+        expect(ok).toBe(true);
       });
     });
 
-    waitsFor(function() {
-      return ok;
-    }, "callback should be called with result=false", 500);
 
-    runs(function() {
-      expect(ok).toBe(true);
+    // ------------------------------------------------------------------------------
+    it("should not be able to open the doc for class ThisClassDoesNotExist", function() {
+      var ok = false;
+      runs(function() {
+        doc.open_class('../output/class_ThisClassDoesNotExist.json', function(result) {
+          ok = !result;
+        });
+      });
+
+      waitsFor(function() {
+        return ok;
+      }, "callback should be called with result=false", 500);
+
+      runs(function() {
+        expect(ok).toBe(true);
+      });
+    });
+
+    // ------------------------------------------------------------------------------
+    it("should be able to push and pop a view", function() {
+      doc.push_view('<div id="test_l">','<div id="test_r">');
+      expect($("#test_l").length).toBe(1);
+      expect($("#test_r").length).toBe(1);
+
+      doc.push_view('<div id="test_l2">','<div id="test_r2">');
+      expect($("#test_l2").length).toBe(1);
+      expect($("#test_r2").length).toBe(1);
+      expect($("#test_l").length).toBe(0);
+      expect($("#test_r").length).toBe(0);
+    
+      doc.pop_view();
+      expect($("#test_l").length).toBe(1);
+      expect($("#test_r").length).toBe(1);
     });
   });
 });
